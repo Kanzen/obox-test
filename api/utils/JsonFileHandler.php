0291470529f41fb4d79688e14cef57b95a4b79d7
Karl Hohls <karl@theterraceinitiative.com>
2024-03-01 22:23:59 +1100

    Add json data store and util for reading and writing to the json file

---
+++ api/utils/JsonFileHandler.php
@@ -0,0 +1,19 @@
+<?php
+class JsonFileHandler 
+{
+    private static $dataDirectory = __DIR__ . '/../data/';
+
+    public static function readJsonFile($fileName) 
+    {
+        $filePath = self::$dataDirectory . $fileName . '.json';
+        $json = file_get_contents($filePath);
+        return json_decode($json, true);
+    }
+
+    public static function writeJsonFile($fileName, $data) 
+    {
+        $filePath = self::$dataDirectory . $fileName . '.json';
+        $json = json_encode($data, JSON_PRETTY_PRINT);
+        file_put_contents($filePath, $json, LOCK_EX);
+    }
+}
